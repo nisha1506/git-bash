4362c0aa928c3456e3fb1fa7aa9a36aa95722496
nisha1506 <nishagoyalmittal11@gmail.com>
2021-03-13 22:07:20 +0530

    changes done

--- sketch.js
+++ sketch.js
@@ -16,8 +16,8 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,25)
-platform = new  Ground(150,305,300,170);
+    ground = new Ground(600,height,1200,15)
+platform = new  Ground(140,300,300,170);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
